c231b918fbbc175722f01e3535b92495cbcefaac
mino <amine222@hotmail.nl>
2024-06-05 00:37:35 +0200

    started working on tests

---
+++ PlusNine.UnitTests/AuthServiceTests.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Moq;
+using PlusNine.DataService.Repositories.Interfaces;
+using PlusNine.Entities.DbSet;
+using PlusNine.Entities.Dtos.Requests;
+using PlusNine.Logic;
+using PlusNine.Logic.Models;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlusNine.UnitTests
+{
+    public class AuthServiceTests
+    {
+
+    }
+}
---
+++ PlusNine.UnitTests/ObjectiveServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Moq;
+using PlusNine.DataService.Repositories.Interfaces;
+using PlusNine.Entities.DbSet;
+using PlusNine.Entities.Dtos.Requests;
+using PlusNine.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlusNine.UnitTests
+{
+    public class ObjectiveServiceTests
+    {
+        private readonly IMapper _mapper;
+
+        public ObjectiveServiceTests()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Objective, Objective>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetAllObjectives_ReturnsUserSpecificObjectives()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var objectiveService = new ObjectiveService(unitOfWorkMock.Object, _mapper);
+
+            var userId = Guid.NewGuid();
+
+            var objectives = new List<Objective>
+            {
+                new() { Id = Guid.NewGuid(), UserId = userId, ObjectiveName = "Objective 1" },
+                new() { Id = Guid.NewGuid(), UserId = userId, ObjectiveName = "Objective 2" },
+                new() { Id = Guid.NewGuid(), UserId = Guid.NewGuid(), ObjectiveName = "Objective 3" }
+            };
+
+            unitOfWorkMock.Setup(uow => uow.Objectives.All()).ReturnsAsync(objectives);
+
+            // Act
+            var result = await objectiveService.GetAllObjectives(userId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+            Assert.All(result, o => Assert.Equal(userId, o.UserId));
+        }
+    }
+}
